052361d19d14e6b3990e25ae9634a1d123ce3b0c
Shahd Hossam <132148556+shahdhoss@users.noreply.github.com>
2024-06-14 13:45:13 +0300

    Time: 11 ms (37.3%), Space: 9.9 MB (5.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t) {
+        unordered_map<int,int> hashmap_s, hashmap_2;
+        for(int i=0;i<s.length();i++){
+            hashmap_s[s[i]]=hashmap_s[s[i]]+1;
+        }
+        for(int i=0;i<t.length();i++){
+            hashmap_2[t[i]]=hashmap_2[t[i]]+1;
+        }
+        if(hashmap_s==hashmap_2){
             return true;
         }
         return false;
