6dd534b37c4cc40960bd4079de5e0c513856f09b
Shahd Hossam <132148556+shahdhoss@users.noreply.github.com>
2024-09-14 08:15:39 +0300

    Time: 84 ms (82.15%), Space: 20.5 MB (46.20%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap={}
+        for string in strs:
+            sorted_string=tuple(sorted(string))
+            if sorted_string not in hashmap:
+                hashmap[sorted_string]=[]
+            hashmap[sorted_string].append(string)
+        return list(hashmap.values())
